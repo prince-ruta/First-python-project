74a98fb121a3cdd84c43e12c26b2ea6216c84565
prince-ruta <rutagengwaprince0@gmail.com>
2024-03-28 09:15:22 +0200

    Create contact and render contact

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import ContactList from './ContactList'
+import ContactForm from './ContactForm'
+
+function App() {
+  const[contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchContacts()
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  }
+
+  return (
+    <>
+      <ContactList contacts={contacts}/>
+      <ContactForm/>
+    </>
+  )
+}
+
+export default App
